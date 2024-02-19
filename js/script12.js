1eb20eac49b7e0c5b7003aacb8d225c9592172cb
mohammadi <ms.mohammadi69@gmail.com>
2024-02-19 15:17:25 +0330

    add file38

---
+++ js/script12.js
@@ -0,0 +1,6 @@
+let inputElem = document.getElementById('input')
+// console.log(inputElem.hasAttribute('placeholder'))
+
+function RemoveAttrHandler (){
+    inputElem.removeAttribute('placeholder')
+}
